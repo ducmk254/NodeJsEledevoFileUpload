0ce64987ad6d294bdab1ec29f565bd292b8172a7
Minh Duc <ducmk254@gmail.com>
2020-10-05 19:43:55 +0700

    setup upload file function

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const express = require('express'),
       app = express(),
       bodyParser = require('body-parser'),
       mongoClient = require('mongoose'),
-      port = process.env.PORT || 3000
+      port = process.env.PORT || 3000,
+      fileUpload = require('express-fileupload')
 mongoClient.connect('mongodb://localhost:27017/fileupload',{useFindAndModify:false,useNewUrlParser:true,useUnifiedTopology:true})
+const path = require('path')
+app.use(fileUpload())
+app.use(express.static(path.join(__dirname, 'fileupload'))) // public thu muc fileupload
+app.post('/upload',  (req,res)=>{
+    if(!req.files || Object.keys(req.files) === 0 )
+        return res.status(400).send('No file were uploaded')
+    let fileU = req.files.foo
+    fileU.mv('./fileupload/'+fileU.name,err=>{
+        if(err)
+            return res.status(500).send('Loi: '+ err)
+        res.send('File uploaded !!!')
+    })
+})
 
 
 
